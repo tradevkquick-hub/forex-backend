36159d9540d0316cf8502a28fb342a59ec7d86f1
TradeVK <trade.vkquick@gmail.com>
2026-05-06 19:52:03 +0530

    fix firebase deploy

--- firebase_config.py
+++ firebase_config.py
@@ -1,6 +1,11 @@
 import firebase_admin
 from firebase_admin import firestore, credentials
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+db = None
+
+try:
+    cred = credentials.Certificate("serviceAccountKey.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+except Exception as e:
+    print("Firebase disabled : ",e)
