0938ba222c05e8d39dd1449758966e9ad1468a6f
TradeVK <trade.vkquick@gmail.com>
2026-05-06 21:17:35 +0530

    fix render database

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 # PostgreSQL connection: replace user, password, dbname
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:S12%40kavya@localhost:5432/forex_backend"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
